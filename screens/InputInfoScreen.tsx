5cd9e179c905010c6c9ba49e2830d92e408150f4
nopehax <57407346+nopehax@users.noreply.github.com>
2022-05-26 17:44:31 +0800

    added more fields

--- screens/InputInfoScreen.tsx
+++ screens/InputInfoScreen.tsx
@@ -11,29 +11,46 @@ export default function InputInfoScreen({ navigation }) {
     <SafeAreaView style={{ flex: 1}}>
       <View style={styles.container}>
         <Text style={{marginBottom: '20'}}>Hi, please input your info below!</Text>
-		    <View style={{flexDirection: 'row'}}>
-			    <Text>myers-briggs personality type:    </Text>
-			    <Picker
-            selectedValue={userType}
-            onValueChange={(itemValue, itemIndex) => setType(itemValue)}>
-            <Picker.Item label='INTP' value='INTP' />
-            <Picker.Item label='INTJ' value='INTJ' />
-            <Picker.Item label='INFP' value='INFP' />
-            <Picker.Item label='INFJ' value='INFJ' />
-            <Picker.Item label='ISTP' value='ISTP' />
-            <Picker.Item label='ISTJ' value='ISTJ' />
-            <Picker.Item label='ISFP' value='ISFP' />
-            <Picker.Item label='ISFJ' value='ISFJ' />
-            <Picker.Item label='ESTJ' value='ESTJ' />
-            <Picker.Item label='ESTP' value='ESTP' />
-            <Picker.Item label='ESFP' value='ESFP' />
-            <Picker.Item label='ESFJ' value='ESFJ' />
-            <Picker.Item label='ENFP' value='ENFP' />
-            <Picker.Item label='ENFJ' value='ENFJ' />
-            <Picker.Item label='ENTP' value='ENTP' />
-            <Picker.Item label='ENTJ' value='ENTJ' />
-          </Picker>
-		    </View>
+	<TextInput
+		value={userName}
+		onChangeText={(userName) => setuserName(userName)}
+		placeholder={'Name'}
+		style={styles.input}
+	/>
+	<View style={{flexDirection: 'row'}}>
+		<Text>which year are you in currently?    </Text>
+		<Picker
+			selectedValue={userYear}
+			onValueChange={(itemValue, itemIndex) => setuserYear(itemValue)}>
+			<Picker.Item label='Y1' value='1' />
+			<Picker.Item label='Y2' value='2' />
+			<Picker.Item label='Y3' value='3' />
+			<Picker.Item label='Y4' value='4' />
+		</Picker>
+	</View>
+	<View style={{flexDirection: 'row'}}>
+		<Text>myers-briggs personality type:    </Text>
+		<Picker
+		    selectedValue={userType}
+		    onValueChange={(itemValue, itemIndex) => setType(itemValue)}>
+		    <Picker.Item label='INTP' value='INTP' />
+		    <Picker.Item label='INTJ' value='INTJ' />
+		    <Picker.Item label='INFP' value='INFP' />
+		    <Picker.Item label='INFJ' value='INFJ' />
+		    <Picker.Item label='ISTP' value='ISTP' />
+		    <Picker.Item label='ISTJ' value='ISTJ' />
+		    <Picker.Item label='ISFP' value='ISFP' />
+		    <Picker.Item label='ISFJ' value='ISFJ' />
+		    <Picker.Item label='ESTJ' value='ESTJ' />
+		    <Picker.Item label='ESTP' value='ESTP' />
+		    <Picker.Item label='ESFP' value='ESFP' />
+		    <Picker.Item label='ESFJ' value='ESFJ' />
+		    <Picker.Item label='ENFP' value='ENFP' />
+		    <Picker.Item label='ENFJ' value='ENFJ' />
+		    <Picker.Item label='ENTP' value='ENTP' />
+		    <Picker.Item label='ENTJ' value='ENTJ' />
+          	</Picker>
+	</View>
 
         <TextInput
               value={userMods}
