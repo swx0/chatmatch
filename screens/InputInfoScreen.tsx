e4a1b5a142fe3a8da2bc0ad25bd7ac87aa5b1f60
nopehax <dynamical36@gmail.com>
2022-05-26 17:23:28 +0800

    add new screen InputInfoScreen

---
+++ screens/InputInfoScreen.tsx
@@ -0,0 +1,70 @@
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { Picker } from '@react-native-picker/picker';
+
+export default function InputInfoScreen({ navigation }) {
+  const [userType, setType] = useState('');
+  const [userMods, setuserMods] = useState('');
+  
+  return (
+    <SafeAreaView style={{ flex: 1}}>
+      <View style={styles.container}>
+        <Text style={{marginBottom: '20'}}>Hi, please input your info below!</Text>
+		    <View style={{flexDirection: 'row'}}>
+			    <Text>myers-briggs personality type:    </Text>
+			    <Picker
+            selectedValue={userType}
+            onValueChange={(itemValue, itemIndex) => setType(itemValue)}>
+            <Picker.Item label='INTP' value='INTP' />
+            <Picker.Item label='INTJ' value='INTJ' />
+            <Picker.Item label='INFP' value='INFP' />
+            <Picker.Item label='INFJ' value='INFJ' />
+            <Picker.Item label='ISTP' value='ISTP' />
+            <Picker.Item label='ISTJ' value='ISTJ' />
+            <Picker.Item label='ISFP' value='ISFP' />
+            <Picker.Item label='ISFJ' value='ISFJ' />
+            <Picker.Item label='ESTJ' value='ESTJ' />
+            <Picker.Item label='ESTP' value='ESTP' />
+            <Picker.Item label='ESFP' value='ESFP' />
+            <Picker.Item label='ESFJ' value='ESFJ' />
+            <Picker.Item label='ENFP' value='ENFP' />
+            <Picker.Item label='ENFJ' value='ENFJ' />
+            <Picker.Item label='ENTP' value='ENTP' />
+            <Picker.Item label='ENTJ' value='ENTJ' />
+          </Picker>
+		    </View>
+
+        <TextInput
+              value={userMods}
+              onChangeText={(userMods) => setuserMods(userMods)}
+              placeholder={'subjects e.g. math, science, english'}
+              style={styles.input}
+            />
+            <Button
+              title="enter"
+              onPress={() => userMods=='' ? {} : navigation.navigate('List', {type: userType, mods: userMods.split(', ')})}
+            />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      padding: '50',
+      backgroundColor: "#0aa",
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    input: {
+        width: 250,
+        height: 44,
+        padding: 10,
+        marginTop: 20,
+        marginBottom: 10,
+        backgroundColor: '#e8e8e8'
+      },
+});
