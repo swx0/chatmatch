0cc4167f155b63ee050c69e73613792668856d85
See Wei Xun <76123658+swx0@users.noreply.github.com>
2022-05-30 03:20:43 +0800

    matchmaking list refreshes

--- screens/MatchMakingScreen.tsx
+++ screens/MatchMakingScreen.tsx
@@ -6,8 +6,11 @@ import React, { useEffect, useState,  } from 'react';
 import { listUsers } from '../src/graphql/queries';
 import { API, Auth, graphqlOperation } from 'aws-amplify';
 import MatchList from '../components/MatchList';
+import { useIsFocused } from '@react-navigation/native';
+
 export default function MatchMakingScreen({ navigation }: RootTabScreenProps<'MatchMaking'>) {
 
+  const isFocused = useIsFocused();
   // List of all registered users
   const [userList, setUserList] = useState(null);
 
@@ -15,19 +18,23 @@ export default function MatchMakingScreen({ navigation }: RootTabScreenProps<'Ma
 
 
   useEffect( () => {
-    const fetchUsers = async () => {
-      try {
-        const usersData = await API.graphql(graphqlOperation(listUsers));
-        const userID = await Auth.currentAuthenticatedUser();
-        setUserList(usersData.data.listUsers.items);
-        setMyUser(userID);
-        
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    fetchUsers(); 
-  }, []);
+    if (isFocused) {
+      setUserList(null);
+      setMyUser(null);
+      const fetchUsers = async () => {
+        try {
+          const usersData = await API.graphql(graphqlOperation(listUsers));
+          const userID = await Auth.currentAuthenticatedUser();
+          setUserList(usersData.data.listUsers.items);
+          setMyUser(userID);
+          
+        } catch (e) {
+          console.log(e);
+        }
+      };
+      fetchUsers();
+    } 
+  }, [isFocused]);
 
   return (userList && myUser ? <MatchList userList={userList} myUser={myUser} navigation={navigation}/>: <Text>Loading</Text>)
 };
